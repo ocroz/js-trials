7de0f1f78e6279f32df6e7070793fe471dacb971
Olivier Crozier <olivier.crozier@nagra.com>
2017-07-05 12:29:11 +0200

    tuto is-node with webpack working

---
+++ tutos/is-node/babelrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    [ 'env', {
+      targets: { browsers: ['> 5%', 'last 2 versions', 'ie 11'] }
+      // targets: { node: 'current' }
+    } ]
+  ]
+}
--- tutos/is-node/webpack.config.js
+++ tutos/is-node/webpack.config.js
@@ -1,24 +1,25 @@
 const path = require('path')
-// const webpack = require('webpack')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/solution2.js',
   output: {
-    filename: 'solution2.js',
+    filename: 'solution2-webpack.js',
     path: path.resolve(__dirname, 'bundle')
   },
+
   module: {
-    preLoaders: [
+    rules: [
       {
-        test: /\.(js|css)$/,
-        loader: 'stripcomments?-lines'
+        test: /\.js$/,
+        include: path.resolve('./src'),
+        loader: 'babel-loader',
+        query: require('./babelrc.js')
       }
     ]
-  } // ,
-  // plugins: [
-    // This one does nothing: webpack uses the browser setting in package.json
-  //   new webpack.NormalModuleReplacementPlugin(new RegExp('./src/lib/index.js'), './src/lib/browser.js'),
-    // This one makes the build to fail
-  //   new webpack.optimize.UglifyJsPlugin({ output: {beautify: true, comments: false, mangle: false} })
-  // ]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({ output: {beautify: true, comments: false} })
+  ].filter(e => e)
 }
