277f640b896c2371fe28e134079df3f263442896
Olivier Crozier <olivier.crozier@nagra.com>
2017-08-24 20:03:39 +0200

    recyclable mylittleapp - remove the useless files

--- recyclable/webnode/mylittleapp/src/controllers/main.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-
-const router = new Router()
-
-router.get('/', (req, res) => res.render('home'))
-
-module.exports = router
--- recyclable/webnode/mylittleapp/step-by-step/server-hello.js
+++ recyclable/webnode/mylittleapp/step-by-step/server-hello.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('port', process.env.PORT || 4000)
+
 app.get('/', (req, res) => { res.send('hello world') })
 
-app.listen(4000)
+app.listen(app.get('port'), () => {
+  console.log('✔ Server listening on port', String(app.get('port')))
+})
